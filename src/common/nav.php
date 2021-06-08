42f20b03cf706f8a7a52c8e4fb87bbfef1afb55c
RekhaDev <rekhalambotte246@gmail.com>
2021-06-08 21:50:17 +0200

    photo membre

--- src/common/nav.php
+++ src/common/nav.php
@@ -1,3 +1,12 @@
+<?php
+    if(isset($_GET["deconnect"]) && ($_GET["deconnect"])== true){
+        $_SESSION["connecté"] = false;
+        session_destroy(); // supprime la session mais ne sera actif lors du chargement de la prochaine page
+        header("location: ./index.php"); // donc on recharge sur la page home
+        exit();
+    }
+?>
+
 <header class="bg">
     <div>
         <a href="../../index.php"><img src="../../src/img/site/logotest.gif" alt="Logo Site Belgium Video-Gaming"></a>
@@ -14,10 +23,38 @@
     </nav>
     <div>
     <nav>
-        <ul>
-            <li><a href="../../src/pages/login.php"><i class="fas fa-sign-in-alt"></i>Login</a></li>
-            <li><a href="../../src/pages/register.php"><i class="fas fa-user-plus"></i>S'enregistrer</a></li>
-        </ul>
+        <?php
+            if(!isset($_SESSION["connecté"]) || $_SESSION["connecté"] ==false ){
+        ?>
+            <ul>
+                <li><a href="../../src/pages/login.php"><i class="fas fa-sign-in-alt"></i>Login</a></li>
+                <li><a href="../../src/pages/register.php"><i class="fas fa-user-plus"></i>S'enregistrer</a></li>
+            
+        <?php
+            }
+            if(isset($_SESSION["connecté"]) && $_SESSION["connecté"] ==true ){
+        ?>
+            <ul>
+                <li><a href="../../src/pages/account.php"><i class="fas fa-user"></i>Mon compte</a></li>
+                <li><a href="../../index.php?deconnect=true"><i class="fas fa-user-alt-slash"></i>Se Déconnecter</a></li>
+            
+
+        <?php
+            }
+            if(isset($_SESSION["user"]["role"]) && $_SESSION["user"]["role"] =="auteur" || isset($_SESSION["user"]["role"]) && $_SESSION["user"]["role"] =="admin"){
+        ?>
+               <li><a href="../../src/pages/rediger.php"><i class="fas fa-user"></i>rédiger</a></li> 
+        <?php
+            }
+            if(isset($_SESSION["user"]["role"]) && $_SESSION["user"]["role"] =="admin"){
+            
+        ?>
+            <li><a href="../../src/pages/admin.php"><i class="fas fa-user-shield"></i>Admin</a></li> 
+        <?php
+            }
+        ?>
+            </ul>
+        
     </nav>
     </div>
 </header>
--- src/pages/account.php
+++ src/pages/account.php
@@ -2,15 +2,82 @@
 
     $titre = "votre compte";
     require "../../src/common/template.php";
-    // require "../../src/fonctions/dbFonction.php";
-    // require "../../src/fonctions/mesFonctions.php";
+    require "../../src/fonctions/dbFonction.php";
+    require "../../src/fonctions/mesFonctions.php";
 
 ?>
 
-<section class="register">
+<section id="account">
+    <div class="account">
+        <div class="infosMembre p-2">
+            <a href="../../src/pages/account.php?edit=true"> 
+                <img title="Cliquez-pour changer votre avatar" src="<?= $_SESSION["user"]["photo"]?>" alt="avatar du membre"> 
+            </a>
+                <!-- Si mon user veut changer le photo -->
+                <?php
+                    if(isset($_GET["edit"]) && $_GET["edit"] == true ){
+                        echo "coucou";
+
+                ?>
+                    <form action="../../src/pages/account.php" method="post" enctype="multipart/form-data">
+                        <input type="file" name="fichier">
+                        <input type="submit" name="Envoyer votre avatar">
+                    </form>
+
+                <?php
+                    }
+                    // Message pour féliciter le t'l'chargement du formulaire
+
+                ?>
+
+                <table>
+                    <tr>
+                        <td> Pseudo : </td>
+                        <td> <?= $_SESSION["user"]["login"] ?></td>
+                    </tr>
+                    <tr>
+                        <td> Nom :</td>
+                        <td>  <?= $_SESSION["user"]["nom"] ?></td>
+                    </tr>
+                    <tr>
+                        <td> Prénom : </td>
+                        <td>  <?= $_SESSION["user"]["prenom"] ?></td>
+                    </tr>
+                    <tr>
+                        <td> Satut :</td>
+                        <td>  <?= $_SESSION["user"]["role"] ?></td>
+                    </tr>
+                
+                </table>
+            
+        </div>
+        <div class="contentMembre p-2">
+            <?php 
+                if($_SESSION["user"]["role"] == "auteur" || $_SESSION["user"]["role"] == "admin"){
+            ?>
+
+            <h2>Vos Articles</h2>
+            <p>Pas d'article</p>
+
+            <?php
+                    }
+            ?>
+
+            <h2>Vos commentaires</h2>
+            <p>Pas de commentaire</p>
+        
+        </div>
+
+    </div>
 
 </section>
 
+<?php
+// traitement du formulaire
+if(isset($_FILES["fichier"])){
+    $photo = sendImg($_FILES["fichier"], "avatar");
+}
+?>
 
 <?php
     require "../../src/common/footer.php";
